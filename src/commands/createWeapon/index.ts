d4b1c223d44b144e29ae1bd1e8bce4d1135382d3
Kreedzt <zhaozisong1@live.com>
2023-08-07 22:52:20 +0800

    :construction: add create weapon base logic

--- src/commands/createWeapon/index.ts
+++ src/commands/createWeapon/index.ts
@@ -1,9 +1,23 @@
 import * as vscode from 'vscode';
 
 export const registerWeaponCommand = (context: vscode.ExtensionContext) => {
-    context.subscriptions.push(vscode.commands.registerCommand('vscode-rwr-mod-tool.createWeapon', () => {
+    context.subscriptions.push(vscode.commands.registerCommand('vscode-rwr-mod-tool.createWeapon', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Create Weapon from rwr-mod-tool!');
+		const inputVal = await vscode.window.showInputBox({
+			title: 'Weapon name',
+			placeHolder: 'Enter your weapon name, do not input .xml suffix',
+			validateInput: (val) => {
+				if (val.trim().length === 0) {
+					return 'Please input weapon name';
+				}
+
+				return '';
+			}
+		});
+
+		vscode.window.showInformationMessage(`Get input: ${inputVal}!`);
+		vscode.window.showInformationMessage(`Get workspace folders: ${vscode.workspace.workspaceFolders?.[0]?.uri}`);
 	}));
 }
