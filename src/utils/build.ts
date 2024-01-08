c52a0efc2007b010cad1ba1de5552ee86de40674
Kreedzt <zhaozisong1@live.com>
2024-01-08 18:28:28 +0800

    :zap: optmize parse/build perf

--- src/utils/build.ts
+++ src/utils/build.ts
@@ -1,15 +1,31 @@
 import { XMLBuilder } from 'fast-xml-parser';
 
-export const buildXML = (content: Record<string, any>): string => {
-    const builder = new XMLBuilder({
-        ignoreAttributes: false,
-        commentPropName: 'comment',
-        // 4 spaces
-        indentBy: '    ',
-        suppressUnpairedNode: false,
-        format: true,
-        suppressEmptyNode: true,
-    });
+const xmlBuilder: {
+    builder: XMLBuilder | null;
+    getBuilder(): XMLBuilder;
+    build: (content: Record<string, any>) => string;
+} = {
+    builder: null,
+    getBuilder() {
+        if (!this.builder) {
+            this.builder = new XMLBuilder({
+                ignoreAttributes: false,
+                commentPropName: 'comment',
+                // 4 spaces
+                indentBy: '    ',
+                suppressUnpairedNode: false,
+                format: true,
+                suppressEmptyNode: true,
+            });
+        }
 
-    return builder.build(content);
+        return this.builder;
+    },
+    build(content: Record<string, any>) {
+        return this.getBuilder().build(content);
+    }
+};
+
+export const buildXML = (content: Record<string, any>): string => {
+    return xmlBuilder.build(content);
 };
