ffcf192f412a18e0a87ce02a196a19b1f6861c0a
Kreedzt <zhaozisong1@live.com>
2025-02-09 22:33:11 +0800

    :zap: optimize filescanner perf

--- packages/vscode/src/services/fileScanner.ts
+++ packages/vscode/src/services/fileScanner.ts
@@ -1,100 +1,84 @@
 import {
+    Observable,
     Subject,
     Subscription,
     debounceTime,
+    distinctUntilChanged,
     from,
-    switchMap,
+    map,
+    mergeMap,
+    shareReplay,
     tap,
 } from 'rxjs';
 import { IValidator } from '../types';
 import { Uri } from 'vscode';
 import * as vscode from 'vscode';
 
+interface FileData {
+    uri: Uri;
+    content: string;
+}
+
 export class FileScanner {
-    filePath2UriMap = new Map<string, Uri>();
-    filePath2ContentMap = new Map<string, string>();
-    subscription = new Subscription();
-    fileChangeSubject = new Subject<string>();
-    validatorList: IValidator[] = [];
-    validateSubject = new Subject<string>();
+    private subscription = new Subscription();
+    private fileSubject = new Subject<FileData>();
+    private validatorList: IValidator[] = [];
+    private validateSubject = new Subject<string>();
+
+    // Share the file stream for multiple subscribers
+    private fileStream$ = this.fileSubject.pipe(
+        distinctUntilChanged((prev, curr) => prev.content === curr.content),
+        debounceTime(250),
+        tap(({ uri }) => this.logFileChange('Processing file', uri.path)),
+        mergeMap(file => this.validateFile(file)),
+        shareReplay(1)
+    );
 
     constructor() {
+        // Subscribe to file processing stream
         this.subscription.add(
-            this.fileChangeSubject
-                .pipe(
-                    debounceTime(250),
-                    tap((path) => this.logFileChange('file change path', path)),
-                    switchMap((path) => {
-                        return from(this.scanFile(path));
-                    }),
-                )
-                .subscribe((file) => this.logFileChange('file change', file)),
+            this.fileStream$.subscribe(path => 
+                this.logFileChange('Completed processing', path)
+            )
         );
     }
 
-    logFileChange(message: string, data: any) {
+    private logFileChange(message: string, data: any) {
         console.log(message, data);
     }
+
     addValidator(validator: IValidator) {
         this.validatorList.push(validator);
-
         return this;
     }
 
-    async validate(fileContent: string, fileUri: Uri) {
-        console.log('validating file', fileUri.path);
-        await Promise.all(
-            this.validatorList.map((v) => v.validate(fileContent, fileUri)),
+    private validateFile(file: FileData): Observable<string> {
+        return from(Promise.all(
+            this.validatorList.map(v => v.validate(file.content, file.uri))
+        )).pipe(
+            tap(() => this.validateSubject.next(file.uri.path)),
+            map(() => file.uri.path)
         );
-        this.validateSubject.next(fileUri.path);
-        return fileUri.path;
-    }
-
-    async scanFile(path: string) {
-        console.log('scanning file', path);
-        const { fileContent, fileUri } = this.getFileContentAndUri(path);
-        if (!fileContent || !fileUri) {
-            console.log('file content not found', path);
-            return;
-        }
-        await this.validate(fileContent, fileUri);
-    }
-
-    getFileContentAndUri(path: string) {
-        const fileContent = this.filePath2ContentMap.get(path);
-        const fileUri = this.filePath2UriMap.get(path);
-        return { fileContent, fileUri };
     }
 
     public async addScanFile(uri: Uri, content?: string) {
-        let realContent = content;
-        if (!realContent) {
-            realContent = await this.readFileContent(uri);
-        }
-        this.filePath2UriMap.set(uri.path, uri);
-
-        console.log('addScanFile', uri.path);
-        const lastContent = this.filePath2ContentMap.get(uri.path);
-        console.log('lastContent diff', lastContent === realContent);
-        if (lastContent === realContent) {
-            return;
-        }
-        this.filePath2ContentMap.set(uri.path, realContent);
-
-        this.fileChangeSubject.next(uri.path);
+        const fileContent = content ?? await this.readFileContent(uri);
+        this.fileSubject.next({
+            uri,
+            content: fileContent
+        });
     }
 
-    async directValidateFile(uri: Uri) {
-        const realContent = await this.readFileContent(uri);
-        this.filePath2UriMap.set(uri.path, uri);
-        this.filePath2ContentMap.set(uri.path, realContent);
-        await this.validate(realContent, uri);
+    public async directValidateFile(uri: Uri) {
+        const content = await this.readFileContent(uri);
+        return this.validateFile({ uri, content }).toPromise();
     }
 
-    async readFileContent(uri: Uri): Promise<string> {
+    private async readFileContent(uri: Uri): Promise<string> {
         const fsContent = await vscode.workspace.fs.readFile(uri);
         return Buffer.from(fsContent).toString();
     }
+
     destroy() {
         this.subscription.unsubscribe();
     }
